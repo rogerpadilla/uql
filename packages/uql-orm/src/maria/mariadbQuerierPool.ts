0af71f61e0d1c1900bbac2084814f827ec362da7
Roger Padilla <sonemonu@gmail.com>
2026-03-08 11:31:37 -0500

    refactor: redefine `PoolConfig` type to derive from `createPool` parameters.

--- packages/uql-orm/src/maria/mariadbQuerierPool.ts
+++ packages/uql-orm/src/maria/mariadbQuerierPool.ts
@@ -1,7 +1,9 @@
-import { createPool, type Pool, type PoolConfig } from 'mariadb';
+import { createPool, type Pool } from 'mariadb';
 import { AbstractQuerierPool } from '../querier/index.js';
 import type { ExtraOptions } from '../type/index.js';
 import { MariadbQuerier } from './mariadbQuerier.js';
+
+type PoolConfig = Exclude<Parameters<typeof createPool>[0], string>;
 
 export class MariadbQuerierPool extends AbstractQuerierPool<MariadbQuerier> {
   readonly pool: Pool;
